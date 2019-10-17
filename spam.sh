c8aa47ceaca8b2abe4116521804d7473a8668fb1
R3f4ll <53784408+R3f4ll@users.noreply.github.com>
2019-10-17 17:20:48 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,159 @@
+#Lo mau Recode?
+#PM dulu kentod !!
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo                         pur='\033[35;1m' #purple                    cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+
+clear
+echo "Assalamualaikum:)" | lolcat
+sleep 2
+echo "Nama gw Mr.R3f4ll , orang terganz:v di masanya..." | lolcat
+sleep 1
+echo ""
+
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+    Mr.R3f4ll
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
+ sleep 1
+echo  $bi "<|======================================|>"
+echo  $bi "<|            Spam tools                |>"
+echo  $bi "<|                                      |>"
+echo  $bi "<| Name: Mr.R3f4ll    ||    Mr.R3f4ll   |>"
+echo  $bi "<| MyTeam: Indo_Darknet_Army            |>"
+echo  $bi "<| Contact: +6282236299010              |>"
+echo  $bi "<| Thanks To: Allah.                    |>"
+echo  $bi "<|======================================|>"
+echo $me "Wait,Loading"
+sleep 3
+echo $i "Ketik help untuk menampilakan bantuan"
+echo $bi "jika ingin melihat menu , ketik menu"
+read -p "===•>" pil;
+
+if [ $pil = help ]
+then
+echo $ku"Cara pemakaian" 
+echo $i"1) ketik nama script yang tersedia di menu"
+echo $pur"2) jika mau exit , ketik keluar"
+echo $bi"3) tools ini isinya spam saja"
+echo $me "GOOD BYE"
+exit
+fi
+
+if [ $pil = menu ]
+then
+echo $i    " MENU           COMMAND"
+echo $i "==============   ============"
+echo $i "SPAMSETAN           SETAN"
+echo $i "BOOM                DUARR"
+echo $i "FAKECALL            FAKE"
+echo $i "LITESPAM            LITE"
+echo $i "exit                EXIT"
+read -p "===•>" ganz;
+fi
+
+if [ $pil = SETAN ]
+then
+echo $me "kode keamanan nya : MRTAMFANX"
+sleep 8
+git clone https://github.com/MrTamfanX/SPAMSETAN
+cd SPAMSETAN
+sh tamfanspam.sh
+fi
+
+if [ $pil = DUARR ]
+then
+git clone https://github.com/pashayogi/boom
+cd boom
+chmod +x start.sh
+./start.sh
+fi
+
+if [ $pil = FAKE ]
+then
+git clone https://github.com/T14RB373T/FAKECALL
+cd FAKECALL
+sh call.sh
+fi
+
+if [ $pil = LITE ]
+then
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+
+
+
+if [ $pil = EXIT ]
+then
+echo $me "makasih ya udah make tools ini"
+echo $bi "DADAH"
+exit
+fi
+
+if [ $ganz = SETAN ]
+then
+echo $me "kode keamanan nya : MRTAMFANX"
+sleep 8
+git clone https://github.com/MrTamfanX/SPAMSETAN
+cd SPAMSETAN
+sh tamfanspam.sh
+fi
+
+if [ $ganz = DUARR ]
+then
+git clone https://github.com/pashayogi/boom
+cd boom
+chmod +x start.sh
+./start.sh
+fi
+
+if [ $ganz = FAKE ]
+then
+git clone https://github.com/T14RB373T/FAKECALL
+cd FAKECALL
+sh call.sh
+fi
+
+if [ $ganz = LITE ]
+then
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $ganz = EXIT ]
+then
+echo $me "makasih ya udah make tools ini"
+echo $bi "DADAH"
+exit
+fi
